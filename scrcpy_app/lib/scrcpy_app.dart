857f885f09775606dd1d70118a96af1f0edb1784
Howard Wang <sunbird.work@gmail.com>
2026-05-07 11:47:15 +0800

    feat: enable light theme and system theme mode in ScrcpyApp

--- scrcpy_app/lib/scrcpy_app.dart
+++ scrcpy_app/lib/scrcpy_app.dart
@@ -1,25 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:scrcpy_app/home_page.dart';
+import 'package:scrcpy_app/theme/app_theme.dart';
 
 class ScrcpyApp extends StatelessWidget {
   const ScrcpyApp({super.key});
-
-  static const _accent = Color(0xFF3B82F6);
-
-  static final _darkTheme = ThemeData(
-    colorSchemeSeed: _accent,
-    brightness: Brightness.dark,
-    scaffoldBackgroundColor: const Color(0xFF0D0D0D),
-  );
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'ScrcpyApp',
       debugShowCheckedModeBanner: false,
-      theme: _darkTheme,
-      darkTheme: _darkTheme,
-      themeMode: ThemeMode.dark,
+      theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
       home: const HomePage(),
     );
   }
